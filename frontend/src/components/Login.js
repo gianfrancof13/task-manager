f04112193f23537beac2131dd6134d896efc4ebf
gianfrancof13 <gianfrancoferrero.e@gmail.com>
2024-09-13 13:54:56 -0300

    Pendientes de guardar

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,31 +4,26 @@ import axios from 'axios';
 const Login = ({ setToken }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null); // Para manejar errores de autenticación
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // Hacer la solicitud POST al backend con el correo y la contraseña
+      //solicitud POST al backend con el correo y la contraseña
       const response = await axios.post('http://backend/api/login', {
         email: email,
         password: password,
       });
 
-      // Verificar si la respuesta contiene un token
       const token = response.data.token;
 
-      // Guardar el token en localStorage y en el estado global
       localStorage.setItem('token', token);
       setToken(token);
-
-      // Redireccionar o realizar alguna acción después del login exitoso
-      console.log('Usuario autenticado con éxito. Token:', token);
-      
+      console.log('Usuario autenticado con éxito. Token:', token);      
     } catch (err) {
       console.error('Error al iniciar sesión:', err);
-      setError('Usuario o contraseña incorrectos.'); // Mensaje de error en la interfaz
+      setError('Usuario o contraseña incorrectos.');
     }
   };
 
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -4,7 +4,6 @@ const TaskList = () => {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    // Simulamos la obtención de tareas
     setTasks([
       { id: 1, title: 'Tarea 1', description: 'Descripción de tarea 1' },
       { id: 2, title: 'Tarea 2', description: 'Descripción de tarea 2' },
